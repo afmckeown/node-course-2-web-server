68b5ac7b0d58bde4dfd23188f74ead0bf55791e5
afmckeown <afmckeown@gmail.com>
2018-10-09 18:48:29 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// use is how we regester middlewhere
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');  
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to Home Page...'
+  });
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'    
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Bad Request.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server up on port 3000.');
+});
